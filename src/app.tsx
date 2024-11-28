57027d1be6ae67e423fbe7f299dc70ee46670b68
Kiwifuit <mahkiwi123@gmail.com>
2024-11-28 20:56:41 +0800

    feat: made other routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,4 @@
+import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
@@ -9,8 +10,11 @@ export default function App() {
     <Router
       root={(props) => (
         <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
+          <MetaProvider>
+            <Title>Law & Order</Title>
+            <Nav />
+            <Suspense>{props.children}</Suspense>
+          </MetaProvider>
         </>
       )}
     >
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,14 +1,26 @@
+import { A } from "@solidjs/router";
+
 export default function Nav() {
   return (
     <nav class="bg-blueberry-200 text-white py-5 px-3 flex gap-6">
-      <img src="/logo.png" class="w-12" />
+      <A href="/">
+        <img src="/logo.png" class="w-12" />
+      </A>
       <div class="flex w-full items-center">
         <ul class="flex gap-3 text-xl italic">
-          <li>What we do</li>
-          <li>How it works</li>
-          <li>About</li>
+          <li>
+            <A href="/what-we-do">What we do</A>
+          </li>
+          <li>
+            <A href="/how-it-works">How it works</A>
+          </li>
+          <li>
+            <A href="/about">About</A>
+          </li>
         </ul>
-        <p class="ml-auto bg-gold-300 p-3">Donate Now!</p>
+        <A href="/donate" class="ml-auto bg-gold-300 p-3">
+          Donate Now!
+        </A>
       </div>
     </nav>
   );
