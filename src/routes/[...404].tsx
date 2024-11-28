d34194ab9a2d4dfa36b4eb900ff15c427089d5c9
Kiwifuit <mahkiwi123@gmail.com>
2024-11-28 21:08:11 +0800

    chore: finished 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,11 +1,19 @@
 // import {Title} from "@solidjs/meta";
 
 import { Title } from "@solidjs/meta";
+import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
-    <main>
+    <main class="grid p-4">
       <Title>Page not Found</Title>
+      <div class="m-auto text-center leading-relaxed">
+        <h1 class="text-8xl font-extralight">Page not found</h1>
+        <p class="italic">Justice is always best served equally</p>
+        <p class="text-blueberry-400">
+          <A href="/">Return to Homepage</A>
+        </p>
+      </div>
     </main>
   );
 }
