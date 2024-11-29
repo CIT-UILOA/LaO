8d84577b26a76eb1dcefc8d3ecfdf721977628b8
Kiwifuit <mahkiwi123@gmail.com>
2024-11-29 20:03:53 +0800

    chore: renamed navbar

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,8 @@ import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
-import Nav from "./components/Nav";
 import Footer from "./components/footer";
+import Nav from "./components/navbar";
 
 export default function App() {
   return (
