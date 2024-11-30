da3b8d6f9a33cce7811e78f41e1ecc92882e7560
Kiwifuit <mahkiwi123@gmail.com>
2024-11-30 17:31:31 +0800

    partial impl for the donation page

--- src/routes/donate.tsx
+++ src/routes/donate.tsx
@@ -1,83 +1,67 @@
-import { createSignal } from "solid-js";
+import { Accessor, createSignal, For, Setter } from "solid-js";
 
 export default function Donate() {
-  const [price, setPrice] = createSignal("");
+  const [amount, setAmount] = createSignal("");
+  const prices = ["10.00", "25.00", "50.00", "100.00", "500.00"];
+
+  const handleTextInputFocus = () => {
+    setAmount("");
+  };
 
   return (
-    <div class="mx-auto w-[700px] my-5 bg-blueberry-100 p-5 rounded-xl text-white">
-      <h1 class="text-5xl">Donate Now!</h1>
-      <div class="flex flex-col gap-3 my-5">
-        <div class="flex gap-3 justify-center">
-          <button
-            class="button"
-            onClick={() => {
-              setPrice("PHP 5.00");
-            }}
-          >
-            PHP 5.00
-          </button>
-          <button
-            class="button"
-            onClick={() => {
-              setPrice("PHP 10.00");
-            }}
-          >
-            PHP 10.00
-          </button>
-          <button
-            class="button"
-            onClick={() => {
-              setPrice("PHP 50.00");
-            }}
-          >
-            PHP 50.00
-          </button>
-          <button
-            class="button"
-            onClick={() => {
-              setPrice("PHP 100.00");
-            }}
-          >
-            PHP 100.00
-          </button>
-          <button
-            class="button"
-            onClick={() => {
-              setPrice("PHP 500.00");
-            }}
-          >
-            PHP 500.00
-          </button>
+    <div class="w-[400px] mx-5 border-blueberry-100 rounded-t-lg border-2 my-5">
+      <h1 class="text-3xl font-light bg-blueberry-100 text-white py-5 px-5 mb-5">
+        Enter amount to donate:
+      </h1>
+      <form action="/" class="">
+        <div class="mx-auto w-64 grid grid-cols-2 gap-3">
+          <For each={prices}>
+            {(price) => (
+              <Radio
+                setAmount={setAmount}
+                getAmount={amount}
+                key="amount"
+                value={price}
+                label={`₱ ${price}`}
+              />
+            )}
+          </For>
         </div>
-        <form action="/">
-          <div class="flex gap-3 ml-20 items-center">
-            <label for="custom-value" class="text-lg">
-              Enter Custom Value:
-            </label>
-            <input
-              id="custom-value"
-              type="text"
-              // bg-orange-50 border border-purple-300 text-gray-900 text-sm rounded-lg block w-full p-2.5
-              class="bg-blueberry-200 placeholder-blueberry-400 text-white p-2.5 rounded-lg"
-              placeholder="PHP 50.00"
-              value={price()}
-              onInput={(e) => setPrice(e.currentTarget.value)}
-            />
-          </div>
-          <input
-            type="submit"
-            class="button block mx-auto w-fit my-5 px-3 rounded-xl"
-            onClick={() => {
-              alert(
-                `AHAHAHHAHAHAH WE STEALING YOUR ${price()} THANK YOU SO MUCH <3333`
-              );
-            }}
-          />
-        </form>
-      </div>
+        <input
+          type="text"
+          onfocus={handleTextInputFocus}
+          placeholder="Custom Value"
+        />
+      </form>
+
       <p class="italic text-sm text-center">
         For non-monetary donations, please contact 0922 414 0700
       </p>
+    </div>
+  );
+}
+
+function Radio(props: {
+  key: string;
+  value: string;
+  label: string;
+  setAmount: Setter<string>;
+  getAmount: Accessor<string>;
+}) {
+  return (
+    <div class="border border-blueberry-400 w-fit h-auto rounded-md font-light has-[:checked]:bg-blueberry-400 has-[:checked]:text-white flex">
+      <input
+        type="radio"
+        id={props.value}
+        name={props.key}
+        value={props.value}
+        checked={props.getAmount() === props.value}
+        onchange={() => props.setAmount(props.value)}
+        class="appearance-none"
+      />
+      <label for={props.value} class="p-5 block">
+        {props.label}
+      </label>
     </div>
   );
 }
