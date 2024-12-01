77f177e363219cc2903198c087c1f5d4eb5ddb4e
Kiwifuit <mahkiwi123@gmail.com>
2024-12-01 14:53:31 +0800

    feat: finalized about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,3 +1,5 @@
+import "./about.css";
+
 export default function About() {
   return (
     <main>
@@ -26,6 +28,56 @@ export default function About() {
           systemic change, Law & Order showed that hunger could be eradicated,
           and justice could be achieved through action and compassion.
         </p>
+      </div>
+      <hr class=" mx-auto my-4 h-1 w-5/6 bg-gold-300 border-none  rounded-xl" />
+      <div class="flex gap-5 mx-auto w-fit">
+        <div>
+          <h2 class="text-4xl">List of Members:</h2>
+          <ul>
+            <li>
+              <h3>Aparte, Ivan Leonard O</h3>
+              <p>President, Simulation + Mockup</p>
+            </li>
+            <li>
+              <h3>Perez, Jon Ivan P</h3>
+              <p>Vice President, Brand Bible + Mockup</p>
+            </li>
+            <li>
+              <h3>Alferez, Ferniel</h3>
+              <p>Concept</p>
+            </li>
+            <li>
+              <h3>Atibula, John Gabriel</h3>
+              <p>Concept</p>
+            </li>
+            <li>
+              <h3>Ibarra, Gian</h3>
+              <p>Concept + Initial Homepage Design</p>
+            </li>
+            <li>
+              <h3>Martus, Julian</h3>
+              <p>Concept</p>
+            </li>
+            <li>
+              <h3>Shawndon, James</h3>
+              <p>Concept</p>
+            </li>
+            <li>
+              <h3>Suerte, Aaliah Ashley</h3>
+              <p>Concept</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 class="text-4xl">Tech Stack</h2>
+          <p class="text-4xl">
+            <i class="nf nf-dev-typescript text-blue-500" />
+            {" + "}
+            <i class="nf nf-dev-solidjs text-blue-600" />
+            {" + "}
+            <i class="nf nf-fa-heart text-red-600" />
+          </p>
+        </div>
       </div>
     </main>
   );
