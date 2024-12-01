e7db136569cd82c69758e981c070399b8a17d515
Kiwifuit <mahkiwi123@gmail.com>
2024-12-01 13:18:16 +0800

    feat: about page

---
+++ src/routes/about.tsx
@@ -0,0 +1,32 @@
+export default function About() {
+  return (
+    <main class="mx-auto my-5 w-3/4">
+      <h1 class="text-6xl">About Us</h1>
+      <div class="leading-relaxed text-justify flex flex-col gap-5 text-lg mx-5">
+        <p>
+          Law & Order is a non-profit organization founded by a group of
+          lawyers, activists, and community leaders with the mission to end
+          world hunger. They began by providing free, nutritious meals to
+          communities in need, but their work went beyond just food
+          distribution. The organization also addressed the root causes of
+          hunger by offering legal aid, advocating for fair wages, promoting
+          sustainable agriculture, and eliminating food waste.
+        </p>
+        <p>
+          As Law & Order grew, it expanded its efforts to include education on
+          food justice and collaborated with local and global organizations to
+          fight hunger through policy change. They also worked with individuals
+          like Carlos, who, after receiving food and legal support, was able to
+          secure a better job and healthcare for his family.
+        </p>
+        <p>
+          The organization faced challenges but remained committed to its cause,
+          ultimately receiving international recognition for its holistic
+          approach to tackling food insecurity. By combining direct aid with
+          systemic change, Law & Order showed that hunger could be eradicated,
+          and justice could be achieved through action and compassion.
+        </p>
+      </div>
+    </main>
+  );
+}
